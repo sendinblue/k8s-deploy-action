a42d52ffa538ed31c4dd1f3f697f080d282f7b69
dimw <dimw@gmx.de>
2021-01-29 15:28:39 +0100

    Introduce action for mock binaries creation

---
+++ .github/sendinblue/mockbin-action/assets/mock.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const mockbinPath = process.env.MOCKBINPATH ? process.env.MOCKBINPATH : './';
+const resultsOutputFile = `${mockbinPath}/mockbin.out`;
+
+// Generate output
+const timestamp = process.hrtime.bigint();
+const [, command] = process.argv;
+const commandName = path.baseName(command);
+
+const result = {
+  "ts": timestamp,
+  "cmd": commandName,
+  "args": process.argv.slice(2),
+};
+
+// Append result
+let results = [];
+try {
+  results = JSON.parse(fs.readFileSync(resultsOutputFile, 'utf8'));
+} catch (e) {
+
+}
+results.push(result);
+
+// Write file
+fs.writeFileSync(resultsOutputFile, JSON.stringify(results));
